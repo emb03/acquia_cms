29991207f2c628a333bdf4f4e9f7cad9803d90be
Elaine Blakeman <elaine.blakeman@bounteous.com>
2022-03-21 14:42:18 -0700

    Plugin for NextConsumerForm -- shows google analytics module TODO: -- Load next consumer entity not a module

---
+++ modules/acquia_cms_headless/src/Plugin/AcquiaCmsHeadless/NextConsumerForm.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Drupal\acquia_cms_headless\Plugin\AcquiaCmsHeadless;
+
+use Drupal\acquia_cms_headless\Form\AcquiaCMSDashboardBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+
+/**
+ * Plugin implementation of the acquia_cms_headless.
+ *
+ * @AcquiaCmsHeadless(
+ *   id = "google_analytics",
+ *   label = @Translation("Google Analytics"),
+ *   weight = 4
+ * )
+ */
+class NextConsumerForm extends AcquiaCMSDashboardBase {
+
+  /**
+   * Provides module name.
+   *
+   * @var string
+   */
+  protected $module = 'google_analytics';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'acquia_cms_next_consumer_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'nextconsumer.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['#tree'] = FALSE;
+    $module = $this->module;
+    if ($this->isModuleEnabled()) {
+      $configured = $this->getConfigurationState();
+      if ($configured) {
+        $form['check_icon'] = [
+          '#prefix' => '<span class= "dashboard-check-icon">',
+          '#suffix' => "</span>",
+        ];
+      }
+
+      $module_path = $this->moduleHandler->getModule($module)->getPathname();
+      $module_info = $this->infoParser->parse($module_path);
+      $form[$module] = [
+        '#type' => 'details',
+        '#title' => $module_info['name'],
+        '#collapsible' => TRUE,
+        '#collapsed' => TRUE,
+      ];
+
+      $form[$module]['web_property_id'] = [
+        '#type' => 'textfield',
+        '#required' => TRUE,
+        '#title' => $this->t('Web Property ID'),
+        '#placeholder' => 'Next.js consumer',
+        '#default_value' => $this->config('nextconsumer.settings')->get('label'),
+        '#prefix' => '<div class= "dashboard-fields-wrapper">' . $module_info['description'],
+        '#suffix' => "</div>",
+      ];
+      $form[$module]['actions']['submit'] = [
+        '#type' => 'submit',
+        '#value' => 'Save',
+        '#prefix' => '<div class= "dashboard-buttons-wrapper">',
+      ];
+      $form[$module]['actions']['ignore'] = [
+        '#type' => 'submit',
+        '#value' => 'Ignore',
+        '#limit_validation_errors' => [],
+        '#submit' => ['::ignoreConfig'],
+      ];
+      if (isset($module_info['configure'])) {
+        $form[$module]['actions']['advanced'] = [
+          '#prefix' => '<div class= "dashboard-tooltiptext">',
+          '#markup' => $this->linkGenerator->generate(
+            'Advanced',
+            Url::fromRoute($module_info['configure'])
+          ),
+          '#suffix' => "</div>",
+        ];
+        $form[$module]['actions']['advanced']['information'] = [
+          '#prefix' => '<b class= "tool-tip__icon">i',
+          '#suffix' => "</b>",
+        ];
+        $form[$module]['actions']['advanced']['tooltip-text'] = [
+          '#prefix' => '<span class= "tooltip">',
+          '#markup' => $this->t("Opens Advance Configuration in new tab"),
+          '#suffix' => "</span></div>",
+        ];
+      }
+      return $form;
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $property_id = $form_state->getValue(['web_property_id']);
+    $this->config('nextconsumer.settings')->set('label', $property_id)->save();
+    $this->state->set('nextconsumer_progress', TRUE);
+    $this->messenger()->addStatus('The configuration options have been saved.');
+
+    // Update state.
+    $this->setConfigurationState();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function ignoreConfig(array &$form, FormStateInterface $form_state) {
+    $this->setConfigurationState();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function checkMinConfiguration() {
+    $label = $this->config('nextconsumer.settings')->get('label');
+    return $label ? TRUE : FALSE;
+  }
+
+}
